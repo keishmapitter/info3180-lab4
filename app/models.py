ee8480352cfc8f88c4c6b9f4cca48650352e1f45
keishma pitter <pitterkeishma@gmail.com>
2024-03-06 22:00:52 -0500

    updated code_2

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128))
 
     def is_authenticated(self):
         return True
@@ -31,3 +32,9 @@ class UserProfile(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+    
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = generate_password_hash(password)
